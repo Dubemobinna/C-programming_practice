ae38f5150e0ab03c0b3a18ebb97e60183b162aae
“Dubem <dubemobinnaesiowu@gmail.com>
2023-02-17 02:32:19 -0800

    simple if-else statement

---
+++ new-hello_world/if-else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - If and else statement
+ * Return: Always 0
+ */
+int main(void)
+{
+	int y = 20;
+
+	if (y == 20)
+	{
+		printf("This is the correct value\n");
+	} else
+	{
+		printf("This is wrong\n");
+	}
+	return (0);
+}
