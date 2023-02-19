f120260c9660f15e33e3e9708d63d16d74e93b46
“Dubem <dubemobinnaesiowu@gmail.com>
2023-02-19 12:00:50 -0800

    Declaring varibles

---
+++ new-hello_world/practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+	num = 2;
+	char rom[] = "Dubem Obinna-Esiowu";
+
+	printf("Duby is %d and I have money\n", num);
+	printf("My name is %s and i just clocked %d today\n", rom, num);
+	return (0);
+}
