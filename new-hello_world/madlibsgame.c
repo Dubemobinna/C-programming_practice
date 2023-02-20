cd0eba1342b7036b812af9b35065614821e9d7dd
“Dubem <dubemobinnaesiowu@gmail.com>
2023-02-20 05:11:04 -0800

    using scanf for two data

--- new-hello_world/madlibsgame.c
+++ new-hello_world/madlibsgame.c
@@ -8,18 +8,19 @@ int main(void)
 {
 	char color[20];
 	char noun[20];
-	char celeb[20];
+	char celebF[20];
+	char celebL[20];
 
 	printf("Enter a color\n");
 	scanf("%s", color);
 	printf("Enter a noun\n");
 	scanf("%s", noun);
 	printf("Enter a celeb\n");
-	scanf("%s", celeb);
+	scanf("%s%s", celebF, celebL);
 
 	printf("Roses are %s\n", color);
 	printf("%s are blue\n", noun);
-	printf("I love %s\n", celeb);
+	printf("I love %s %s\n", celebF, celebL);
 
 	return (0);
 }
