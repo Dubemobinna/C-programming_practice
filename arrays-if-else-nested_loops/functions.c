a830adf4569c8958c3e454a214784f475185ac92
“Dubem <dubemobinnaesiowu@gmail.com>
2023-03-04 09:34:15 -0800

    Calling a function

---
+++ arrays-if-else-nested_loops/functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - Functions
+ * Return: Always 0
+ */
+
+void SayHi()
+{
+	printf("Hello User\n");
+}
+
+int main()
+{
+	printf("This is my territory\n");
+	SayHi();
+	return (0);
+}
