d57959ffae4166034c65dddbefceaa26756d477d
“Dubem <dubemobinnaesiowu@gmail.com>
2023-03-05 04:51:09 -0800

    multiple parameters for a function

--- arrays-if-else-nested_loops/functions.c
+++ arrays-if-else-nested_loops/functions.c
@@ -4,16 +4,16 @@
  * Return: Always 0
  */
 
-void SayHi(char name[])
+void SayHi(char name[], int age)
 {
-	printf("Hello %s\n", name);
+	printf("Hello %s, you will be %d next year\n", name, age);
 }
 
 int main()
 {
 	printf("This is my territory\n");
-	SayHi("Mike");
-	SayHi("John");
-	SayHi("Attah");
+	SayHi("Mike", 40);
+	SayHi("John", 35);
+	SayHi("Attah", 60);
 	return (0);
 }
